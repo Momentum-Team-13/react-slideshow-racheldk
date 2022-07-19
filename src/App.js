a4ef33db0b3a094d8b08b7040d37ef9f5a821f6f
Rachel Kelly <racheldk126@gmail.com>
2022-07-19 07:38:10 -0700

    fetch request and buttons working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import * as data from './film-data.json'
+// import * as data from './film-data.json'
 import Slideshow from './Slideshow'
 import 'materialize-css/dist/css/materialize.css'
 
 
 function App() {
-  return (
+  const [data, setData] =useState(null);
+  const getData=()=>{
+    fetch('film-data.json'
+    ,{
+      headers : {
+        'Content-Type': 'appliation/json',
+        'Accept': 'application/json'
+      }
+    }
+    )
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(myJson) {
+        setData(myJson)
+      })
+  }
+
+  useEffect(() =>{
+    getData()
+  }, [])
+
+  return (  
     <div>
-      <Slideshow />
+      {/* <Slideshow /> */}
+      {data? <Slideshow FILMS={data}/> : <h3>Loading...</h3> }
     </div>
   )
 }
